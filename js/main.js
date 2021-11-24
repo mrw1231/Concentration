7ea239916983293c93e05b28b486ce18f7917be1
Michael White <michael@Michaels-MacBook-Pro.local>
2021-11-23 17:13:20 -0700

    Committing CSS Changes

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*----- constants -----*/
+const backOfCardImg = 'https://i.imgur.com/nIaRaqU.jpg'
+
+const pairOneImg =
+const pairTwoImg =
+const pairThreeImg =
+const pairFourImg =
+const pairFiveImg =
+const pairSixImg = 
+const pairSevenImg = 
+const pairEightImg = 
+
+/*----- app's state (variables) -----*/
+let board;
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
