d5dd1e09383e771efc73101f78610afc08f55a1b
Michael White <michael@Michaels-MacBook-Pro.local>
2021-11-29 19:57:47 -0700

    made game board

--- js/main.js
+++ js/main.js
@@ -1,23 +1,67 @@
 /*----- constants -----*/
-const backOfCardImg = 'https://i.imgur.com/nIaRaqU.jpg'
+const backOfCardImg = 'https://i.imgur.com/nIaRaqU.jpg';
 
-const pairOneImg =
-const pairTwoImg =
-const pairThreeImg =
-const pairFourImg =
-const pairFiveImg =
-const pairSixImg = 
-const pairSevenImg = 
-const pairEightImg = 
+const pairs = [
+    { id: '1', imgSrc: "https://i.imgur.com/tSdaDmw.png"},
+    { id: '2', imgSrc: "https://i.imgur.com/OQSe65W.png"},
+    { id: '3', imgSrc: "https://i.imgur.com/nNIIpJX.png"},
+    { id: '4', imgSrc: "https://i.imgur.com/w4JvUkg.png"},
+    { id: '5', imgSrc: "https://i.imgur.com/MuZakHJ.png"},
+    { id: '6', imgSrc: "https://i.imgur.com/1CJTKn1.png"},
+    { id: '7', imgSrc: "https://i.imgur.com/wRHmbh7.png"},
+    { id: '8', imgSrc: "https://i.imgur.com/W8fgJ5r.png"},
+    { id: '1', imgSrc: "https://i.imgur.com/tSdaDmw.png"},
+    { id: '2', imgSrc: "https://i.imgur.com/OQSe65W.png"},
+    { id: '3', imgSrc: "https://i.imgur.com/nNIIpJX.png"},
+    { id: '4', imgSrc: "https://i.imgur.com/w4JvUkg.png"},
+    { id: '5', imgSrc: "https://i.imgur.com/MuZakHJ.png"},
+    { id: '6', imgSrc: "https://i.imgur.com/1CJTKn1.png"},
+    { id: '7', imgSrc: "https://i.imgur.com/wRHmbh7.png"},
+    { id: '8', imgSrc: "https://i.imgur.com/W8fgJ5r.png"},
+]
 
 /*----- app's state (variables) -----*/
-let board;
+
 
 /*----- cached element references -----*/
-
+const section = document.querySelector('section');
 
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+init();
+
+function init() {
+    shuffle(pairs);
+    pairs.forEach(function(pair, idx) {
+        const card = document.createElement('div');
+        const front = document.createElement('img');
+        const back = document.createElement('div');
+        card.classList = 'card';
+        front.classList = 'front';
+        back.classList = 'back';
+        front.src = pairs[idx].imgSrc;
+        section.appendChild(card);
+        card.appendChild(front);
+        card.appendChild(back);
+    });
+    render();
+};
+  
+ function render() {
+    
+ } 
+
+function shuffle(value) {
+    let index = value.length,  randomIndex;
+    while (index != 0) {
+      randomIndex = Math.floor(Math.random() * index);
+      index--;
+      [value[index], value[randomIndex]] = [
+        value[randomIndex], value[index]];
+    }
+    return value;
+  }
+
 
